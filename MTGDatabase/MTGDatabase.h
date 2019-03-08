983a85599034254a21901bf16fd51dbfc61f01ea
Kyle Hankinson <kyle@hankinsoft.com>
2019-03-08 14:20:14 -0400

    + Initial

---
+++ MTGDatabase/MTGDatabase.h
@@ -0,0 +1,19 @@
+//
+//  MTGDatabase.h
+//  MTGDatabase
+//
+//  Created by Kyle Hankinson on 2018-10-22.
+//  Copyright © 2018 Hankinsoft Development, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTGDatabase.
+FOUNDATION_EXPORT double MTGDatabaseVersionNumber;
+
+//! Project version string for MTGDatabase.
+FOUNDATION_EXPORT const unsigned char MTGDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTGDatabase/PublicHeader.h>
+
+
